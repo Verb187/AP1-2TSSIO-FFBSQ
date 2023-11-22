94f93eba8ac9946abd1b9ef97d8d2d0c801be489
Mattbrn <mbricman3@gmail.com>
2023-11-22 12:35:47 +0100

    Update index.php
    - Modification du titre d'index

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page de connexion/inscription - FFBSQ</title>
+    <title>Page de connexion/inscription  FFBSQ</title>
     <!-- Inclure Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>

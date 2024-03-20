045cf334a799704270539933b7396338a79fe66f
VerbDev <58372226+Verb187@users.noreply.github.com>
2024-03-20 10:11:29 +0100

    test

---
+++ View/licence/creation_licence.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../assets/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actions - FFBSQ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<body>
+    <h2 class="text-center mb-4">Création des Licenciés</h2>
+    
